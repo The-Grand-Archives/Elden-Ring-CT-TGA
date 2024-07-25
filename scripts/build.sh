4166cb9fea3313325ac114ffaa831b24ca8e91f7
William Tremblay <tremwil@gmail.com>
2024-07-24 21:16:38 -0400

    install ce2fs requirements

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 mkdir ./dist
 echo 1245620 >./dist/steam_appid.txt
+pip install -r ./ce2fs/requirements.txt
 python ./ce2fs/ce2fs.py -i ./CheatTable -o ./dist/ER_TGA_v$(sed -r 's/([0-9])$/.\1/' VERSION).CT "$@"
--- scripts/check.sh
+++ scripts/check.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+pip install -r ./ce2fs/requirements.txt
 python ./ce2fs/ce2fs.py -i ./CheatTable --check "$@"
