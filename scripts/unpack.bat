d40a139952ffae2285532429294c704d8e6b0c17
William Tremblay <tremwil@gmail.com>
2024-07-27 00:17:25 -0400

    remove output dir from unpack scripts for now

--- scripts/unpack.bat
+++ scripts/unpack.bat
@@ -1,3 +1,3 @@
 pip install -r ./ce2fs/requirements.txt
 for /f %%i in ('where .\dist:ER_TGA_v*.CT') do set RESULT=%%i
-python ./ce2fs/ce2fs.py -i "%RESULT%" -o .
+python ./ce2fs/ce2fs.py -i "%RESULT%" %*
--- scripts/unpack.sh
+++ scripts/unpack.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 pip install -r ./ce2fs/requirements.txt
-python ./ce2fs/ce2fs.py -i $(find ./dist -name 'ER_TGA_v*.CT') -o .
+python ./ce2fs/ce2fs.py -i $(find ./dist -name 'ER_TGA_v*.CT') "$@"
