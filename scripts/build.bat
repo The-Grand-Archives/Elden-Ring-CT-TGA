0a3e901923206bef519d3af295522da7b48fedf3
William Tremblay <tremwil@gmail.com>
2024-07-27 00:17:01 -0400

    rewrite build script in python to avoid pain of windows

--- scripts/build.bat
+++
@@ -1,7 +0,0 @@
-set /p ver=<VERSION
-set ver_sem=%ver:~0,-1%.%ver:~-1%
-mkdir .\dist
-echo 1245620 >.\dist\steam_appid.txt
-pip install -r .\ce2fs\requirements.txt
-python .\scripts\pack_table_files.py
-python .\ce2fs\ce2fs.py -i .\CheatTable -o .\dist\ER_TGA_v%ver_sem%.CT %*
--- scripts/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-mkdir ./dist
-echo 1245620 >./dist/steam_appid.txt
-pip install -r ./ce2fs/requirements.txt
-python ./scripts/pack_table_files.py
-python ./ce2fs/ce2fs.py -i ./CheatTable -o ./dist/ER_TGA_v$(sed -r 's/([0-9])$/.\1/' VERSION).CT "$@"
